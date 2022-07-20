573a00aad8c9d79fafecb592f273b719cc153d41
Ilja <mushketer888@gmail.com>
2022-07-20 21:59:08 +0300

    Initial

---
+++ convert.py
@@ -0,0 +1,19 @@
+import os
+import wave
+
+for file in os.listdir():
+    if file.endswith(".VR8"):
+        print('Converting file %s' % file)
+        with open(file, "rb") as inp_f:
+            data = inp_f.read()
+            new_filename = file + ".wav"
+            out_f = wave.open(new_filename, "wb")
+            out_f.setnchannels(1)
+            out_f.setsampwidth(2) # number of bytes
+            out_f.setframerate(44100)
+            out_f.writeframesraw(data)
+            out_f.close()
+            print('Succesfully created WAV file %s' % file)
+
+print('Press enter to exit...')
+input()
